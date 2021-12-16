209d4fc630807d481c1072bbca5e9dbb594159a3
Hackermans <lukdam22@bergen.org>
2021-12-16 11:34:56 -0500

    yes

---
+++ data/dummyprojects.js
@@ -0,0 +1,112 @@
+module.exports =  [
+	{
+		"project_id" : 1,
+		"project_name" : "BCA Mart",
+		"project_location" : "Auditorium",
+		"project_type" : "Main",
+		"project_date_ymd" : "2021-08-03",
+		"project_date" : "August 3 (Tuesday)",
+		"project_time" : "11:30 AM",
+		"project_duration" : 30,
+		"project_interest" : 11
+	},
+	{
+		"project_id" : 2,
+		"project_name" : "StuCo",
+		"project_location" : "Zhang's Room",
+		"project_type" : "Main",
+		"project_date_ymd" : "2021-09-03",
+		"project_date" : "September 03 (Sun)",
+		"project_time" : "11:30 AM",
+		"project_duration" : null,
+		"project_interest" : 1
+	},
+	{
+		"project_id" : 3,
+		"project_name" : "Debate",
+		"project_location" : "Gym",
+		"project_type" : "Main",
+		"project_date_ymd" : "2021-05-04",
+		"project_date" : "May 04 (Mon)",
+		"project_time" : "9:30 AM",
+		"project_duration" : 31,
+		"project_interest" : 2
+	},
+	{
+		"project_id" : 4,
+		"project_name" : "Baseball",
+		"project_location" : "Field",
+		"project_type" : "Main",
+		"project_date_ymd" : "2021-05-12",
+		"project_date" : "May 12 (Tuesday)",
+		"project_time" : "11:45 AM",
+		"project_duration" : 33,
+		"project_interest" : 13
+	},
+	{
+		"project_id" : 5,
+		"project_name" : "Video Games",
+		"project_location" : "Lower Caf",
+		"project_type" : "Workshop",
+		"project_date_ymd" : "2021-05-02",
+		"project_date" : "May 02 (Sun)",
+		"project_time" : "12:30 PM",
+		"project_duration" : 96,
+		"project_interest" : 15
+	},
+	{
+		"project_id" : 6,
+		"project_name" : "Not running",
+		"project_location" : "Room 130A",
+		"project_type" : "Workshop",
+		"project_date_ymd" : "2021-05-17",
+		"project_date" : "May 17 (Sun)",
+		"project_time" : "5:30 PM",
+		"project_duration" : 43,
+		"project_interest" : 03
+	},
+	{
+		"project_id" : 7,
+		"project_name" : "Cardio",
+		"project_location" : "Room 130B",
+		"project_type" : "Workshop",
+		"project_date_ymd" : "2021-05-19",
+		"project_date" : "May 019 (Sun)",
+		"project_time" : "12:30 PM",
+		"project_duration" : 72,
+		"project_interest" : 4
+	},
+	{
+		"project_id" : 8,
+		"project_name" : "Super Smash Brothers",
+		"project_location" : "Room 137A",
+		"project_type" : "Workshop",
+		"project_date_ymd" : "2021-05-22",
+		"project_date" : "May 22 (Sun)",
+		"project_time" : "2:30 PM",
+		"project_duration" : 61,
+		"project_interest" : 6
+	},
+	{
+		"project_id" : 9,
+		"project_name" : "Coding for Funsies",
+		"project_location" : "Room 138B",
+		"project_type" : "Workshop",
+		"project_date_ymd" : "2021-05-10",
+		"project_date" : "May 10 (Sun)",
+		"project_time" : "4:00 PM",
+		"project_duration" : 42,
+		"project_interest" : 1
+	},
+	{
+		"project_id" : 10,
+		"project_name" : "Computer Science",
+		"project_location" : "Room 136",
+		"project_type" : "Workshop",
+		"project_date_ymd" : "2021-05-30",
+		"project_date" : "May 30 (Sun)",
+		"project_time" : "4:00 PM",
+		"project_duration" : 49,
+		"project_interest" : 3
+	},
+]
